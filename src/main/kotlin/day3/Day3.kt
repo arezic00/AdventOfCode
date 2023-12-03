66dd61321e64119a337b0bc1cddb259c00635548
Antonio Rezic <antonio.rezic00@gmail.com>
2023-12-03 18:02:26 +0100

    Solve part 2

--- src/main/kotlin/day3/Day3.kt
+++ src/main/kotlin/day3/Day3.kt
@@ -1,26 +1,34 @@
 package day3
 
 import java.io.File
-import java.nio.charset.Charset
 
 fun main() {
-    Day3().solvePart1("ChallengeResources/day3_puzzle_input.txt")
+    val day3 = Day3("ChallengeResources/day3_puzzle_input.txt")
+    day3.solvePart1()
+    day3.solvePart2()
 }
 
-class Day3 {
-    private fun parse(lines: List<String>): MutableList<Number> {
-        val numbers = mutableListOf<Number>()
-        var prePreviousIndexedSymbols = mutableMapOf<Int,Char>()
+class Day3(filename: String) {
+    private val numbers = mutableListOf<Number>()
+    private val gears = mutableListOf<Gear>()
+
+    init {
+        parse(File(filename).readLines())
+    }
+
+    private fun parse(lines: List<String>) {
+        var prePreviousNumbers = mutableMapOf<Int, String>()
+        var prePreviousIndexedSymbols = mutableMapOf<Int, Char>()
         var previousNumbers = mutableMapOf<Int, String>()
-        var previousIndexedSymbols = mutableMapOf<Int,Char>()
+        var previousIndexedSymbols = mutableMapOf<Int, Char>()
         lines.forEach { line ->
             val indexedNumbers = mutableMapOf<Int, String>()
-            val indexedSymbols = mutableMapOf<Int,Char>()
+            val indexedSymbols = mutableMapOf<Int, Char>()
             var i = 0
             while (i < line.length) {
                 if (line[i] != '.') {
                     if (!(line[i].isDigit())) {
-                        indexedSymbols[i++] = line[i]
+                        indexedSymbols[i] = line[i++]
                         continue
                     }
                     val index = i
@@ -32,36 +40,66 @@ class Day3 {
                     indexedNumbers[index] = numberString
                 } else i++
             }
+
             previousNumbers.forEach {
                 val leftIndex = it.key - 1
                 val rightIndex = it.key + it.value.length
                 val isPartNumber =
                     prePreviousIndexedSymbols.any { it.key in leftIndex..rightIndex } ||
-                    previousIndexedSymbols.any { it.key == leftIndex || it.key == rightIndex } ||
-                    indexedSymbols.any { it.key in leftIndex..rightIndex }
-                numbers.add(Number(it.value.toInt(),isPartNumber))
+                            previousIndexedSymbols.any { it.key == leftIndex || it.key == rightIndex } ||
+                            indexedSymbols.any { it.key in leftIndex..rightIndex }
+
+                numbers.add(Number(it.value.toInt(), isPartNumber))
             }
+            previousIndexedSymbols.filter { it.value == '*' }
+                .forEach {
+                    val leftIndex = it.key - 1
+                    val rightIndex = it.key + 1
+                    val adjacentNumbers =
+                        prePreviousNumbers.filter { it.key in leftIndex..rightIndex || it.key + it.value.length - 1 in leftIndex..rightIndex }.map { it.value.toInt() } +
+                                previousNumbers.filter { it.key == rightIndex || it.key + it.value.length - 1 == leftIndex }.map { it.value.toInt() } +
+                                indexedNumbers.filter { it.key in leftIndex..rightIndex || it.key + it.value.length - 1 in leftIndex..rightIndex }.map { it.value.toInt() }
+                    if (adjacentNumbers.size == 2) gears.add(Gear(adjacentNumbers[0],adjacentNumbers[1]))
+                }
+
             prePreviousIndexedSymbols = previousIndexedSymbols
             previousIndexedSymbols = indexedSymbols
+            prePreviousNumbers = previousNumbers
             previousNumbers = indexedNumbers
         }
+
         previousNumbers.forEach {
             val leftIndex = it.key - 1
             val rightIndex = it.key + it.value.length
             val isPartNumber =
                 prePreviousIndexedSymbols.any { it.key in leftIndex..rightIndex } ||
-                previousIndexedSymbols.any { it.key == leftIndex || it.key == rightIndex }
-            numbers.add(Number(it.value.toInt(),isPartNumber))
+                        previousIndexedSymbols.any { it.key == leftIndex || it.key == rightIndex }
+            numbers.add(Number(it.value.toInt(), isPartNumber))
         }
-        return numbers
+        previousIndexedSymbols.filter { it.value == '*' }
+            .forEach {
+                val leftIndex = it.key - 1
+                val rightIndex = it.key + 1
+                val adjacentNumbers =
+                    prePreviousNumbers.filter { it.key in leftIndex..rightIndex || it.key + it.value.length -1 in leftIndex..rightIndex }.map { it.value.toInt() } +
+                            previousNumbers.filter { it.key == rightIndex || it.key + it.value.length - 1 == leftIndex }.map { it.value.toInt() }
+                if (adjacentNumbers.size == 2) gears.add(Gear(adjacentNumbers[0],adjacentNumbers[1]))
+            }
     }
 
-    fun solvePart1(filename: String) =
-        println(parse(File(filename).readLines(Charset.defaultCharset()))
+    fun solvePart1() =
+        println(numbers
             .filter { it.isPartNumber }
             .sumOf { it.value })
+
+    fun solvePart2() =
+        println(gears.sumOf { it.ratio() })
+
+    private fun Gear.ratio() = this.partNumber1 * this.partNumber2
 }
 
 
 data class Number(val value: Int, val isPartNumber: Boolean = false)
+
+data class Gear(val partNumber1: Int, val partNumber2: Int)
 
